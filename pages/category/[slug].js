0f1adf84cfcbe55bd5bcb265ff8a89790e4b5378
ErisFender <tarkkusss@gmail.com>
2022-10-24 17:49:12 -0300

    Adding title to category page

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -2,9 +2,10 @@ import React from 'react';
 import { useRouter } from 'next/router';
 
 import { getCategories, getCategoryPost } from '../../services';
-import { PostCard, Categories, Loader } from '../../components';
+import {PostCard, Categories, Loader} from '../../components';
+import Head from "next/head";
 
-const CategoryPost = ({ posts }) => {
+const CategoryPost = ({ posts, slug }) => {
     const router = useRouter();
 
     if (router.isFallback) {
@@ -12,16 +13,22 @@ const CategoryPost = ({ posts }) => {
     }
 
     return (
-        <div className="container mx-auto px-10 mb-8">
-            <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-                <div className="col-span-1 lg:col-span-8">
-                    {posts.map((post, index) => (
-                        <PostCard key={index} post={post.node} />
-                    ))}
-                </div>
-                <div className="col-span-1 lg:col-span-4">
-                    <div className="relative lg:sticky top-8">
-                        <Categories />
+
+        <div>
+            <Head>
+                <title>{slug.split("-").join(" ").toLocaleUpperCase()}</title>
+            </Head>
+            <div className="container mx-auto px-10 mb-8">
+                <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+                    <div className="col-span-1 lg:col-span-8">
+                        {posts.map((post, index) => (
+                            <PostCard key={index} post={post.node}/>
+                        ))}
+                    </div>
+                    <div className="col-span-1 lg:col-span-4">
+                        <div className="relative lg:sticky top-8">
+                            <Categories />
+                        </div>
                     </div>
                 </div>
             </div>
@@ -29,22 +36,14 @@ const CategoryPost = ({ posts }) => {
     );
 };
 export default CategoryPost;
-
 // Fetch data at build time
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
     const posts = await getCategoryPost(params.slug);
 
     return {
-        props: { posts },
+        props: { posts, slug: params.slug },
     };
 }
 
 // Specify dynamic routes to pre-render pages based on data.
 // The HTML is generated at build time and will be reused on each request.
-export async function getStaticPaths() {
-    const categories = await getCategories();
-    return {
-        paths: categories.map(({ slug }) => ({ params: { slug } })),
-        fallback: true,
-    };
-}
